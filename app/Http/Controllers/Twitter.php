5045824d2e2c3b0762aad166ecfdf63f327c8e01
yuu oka <yuu.okd.hotmail@gmail.com>
2020-10-14 15:36:20 +0900

    ループ処理成功

--- app/Http/Controllers/Twitter.php
+++ app/Http/Controllers/Twitter.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Abraham\TwitterOAuth\TwitterOAuth;
+use Illuminate\Support\Facades\Log;
 
 class Twitter extends Controller
 {
@@ -17,6 +18,9 @@ class Twitter extends Controller
 
   public function search(Request $request)
   {
+
+    // 処理速度の計測開始
+    // $st = microtime(true);
 
     // URLで検索した場合、構成されているURLからtweetID,tweetUserに分解
     if( isset($request->button3) ){
@@ -39,6 +43,7 @@ class Twitter extends Controller
     $roop_count = 0;
     $arrayDataS = array();
     $arrayDataM = array();
+    $gasSetText = array();
 
     // APIの検索キーのセット
     if( empty( $request->searchWord ) ){
@@ -50,21 +55,16 @@ class Twitter extends Controller
     }
 
     // 一回100件までしか取得できないので取得件数が400まで,10回ループまで繰り返す。
-    while ( $tweet_count < 100 and $roop_count < 2):
+    while ( $tweet_count < 300 and $roop_count < 4):
       // APIの呼び出し
       // $tweets = $connection->get("search/tweets", ['q' => $params , 'count' => 20] );
       $tweets = $connection->get("search/tweets", $params );
 
       // エラーが出てきたらddして中断。
-      if( empty($tweets->errors) ){
-      } else {
-        dd($tweets);
-      }
+      if( empty($tweets->errors) ){} else {dd("エラーが出現しました。" . $tweets);}
 
       // 1週間以内にtweetがないとstatusesが0になるので0が返ってきたときは繰り返しを終了させる。
-      if( count($tweets->statuses) == 0 ){
-        $tweet_count = 99999;
-      }
+      if( count($tweets->statuses) == 0 ){$tweet_count = 99999;}
 
       // 取ってきたreturn件数を繰り返し判定に加算
       $tweet_count += count($tweets->statuses);
@@ -75,7 +75,8 @@ class Twitter extends Controller
         $tweet_count = 99999;
       }else{
         // next_results に 検索parameter + 取得件数　を含む際呼び出しパラメータが設定されているのでセット。
-        $params['next'] =  preg_replace('/^\?/', '', $tweets->search_metadata->next_results);
+        $next_results = preg_replace('/^\?/', '', $tweets->search_metadata->next_results);
+        parse_str($next_results, $params);
       }
 
       $roop_count++;
@@ -87,6 +88,8 @@ class Twitter extends Controller
     // 取得したデータを逆順でcollectionにセット（最新順に取得されてしまうため。）
     $tweets->statuses = array_reverse($arrayDataS);
     $tweets->search_metadata = array_reverse($arrayDataM);
+    // dd($tweets->search_metadata);
+
 
     if( empty($request->{'searchWord'}) ){
       $return = array();
@@ -101,41 +104,44 @@ class Twitter extends Controller
           // 戻す際に日付をctime形式からymdに変換
           $tweet->created_at = date("Y-m-d h:i:s",strtotime($tweet->created_at));
 
-          // ついでにgoogle翻訳もかけておく
-          // curlコマンドで実行するとき。
-          // curl -d '{"text":"hello", "source":"en","target":"ja"}' -L https://script.google.com/macros/s/AKfycbySxRYYZ_23oXT1gn-4d2mkXhk6f9gj6ywynCKmAASiojvvroZf/exec
-
-          $params = array('text' => $tweet->text,'source' => "en",'target' => "ja");
-          $sendURL = "https://script.google.com/macros/s/AKfycbySxRYYZ_23oXT1gn-4d2mkXhk6f9gj6ywynCKmAASiojvvroZf/exec";
-
-          $curl = curl_init($sendURL);      // curlの処理を始める合図
-          curl_setopt($curl, CURLOPT_POST, true); //POST送信
-          curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($params)); //postするデータの格納（json）
-          curl_setopt($curl,CURLOPT_FOLLOWLOCATION,true);//Locationをたどる
-          curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);//文字列で結果を返させる
-          $response = curl_exec($curl);// レスポンスを変数に入れる
-          curl_close($curl);// curlの処理を終了
-
-          $returnAPI = json_decode( $response ) ;
-
-          // $returnParts = array('id' => $tweet->id,'created_at' => $tweet->created_at,'text' => $tweet->text , 'textJp' => $returnAPI->message, 'user'->screen_name => $tweet->user->screen_name);
           // 通信量削減のため戻す項目を絞る。絞らない場合は textJpを$tweetにセットするだけ。
           $returnParts = array();
           $returnParts['id'] = $tweet->id;
           $returnParts['created_at'] = $tweet->created_at;
           $returnParts['text'] = $tweet->text;
-          $returnParts['textJp'] = $returnAPI->message;
           $returnParts['screen_name'] = $tweet->user->screen_name;
 
+          array_push( $gasSetText , $tweet->text );
           array_push( $return , (object)$returnParts );
 
-          // 項目絞らない判。どっちがはやいかわからん。（viewのscreen_nameをuser->に直す必要がある。）
-          // $tweet->textJp = $returnAPI->message;
-          // array_push( $return , $tweet );
         }
       }
       $tweets =  (object)['statuses' => $return];
     }
+
+    // oogle翻訳もかけておく
+    // curlコマンドで実行するとき。
+    // curl -d '{"text":"hello", "source":"en","target":"ja"}' -L https://script.google.com/macros/s/AKfycbySxRYYZ_23oXT1gn-4d2mkXhk6f9gj6ywynCKmAASiojvvroZf/exec
+    $params = array('text' => $gasSetText,'source' => "en",'target' => "ja");
+    $sendURL = "https://script.google.com/macros/s/AKfycbySxRYYZ_23oXT1gn-4d2mkXhk6f9gj6ywynCKmAASiojvvroZf/exec";
+
+    $curl = curl_init($sendURL);      // curlの処理を始める合図
+    curl_setopt($curl, CURLOPT_POST, true); //POST送信
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($params)); //postするデータの格納（json）
+    curl_setopt($curl,CURLOPT_FOLLOWLOCATION,true);//Locationをたどる
+    curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);//文字列で結果を返させる
+    $response = curl_exec($curl);// レスポンスを変数に入れる
+    curl_close($curl);// curlの処理を終了
+
+    $returnAPI = json_decode( $response ) ;
+    // GASにから返ってきた情報を配列にセット
+    foreach ($tweets->statuses as $idx => $tweet){
+      $tweet->textJp = $returnAPI->message[$idx];
+    }
+
+    // 処理速度の計測終了
+    // $now = microtime(true);
+    // Log::debug('○○の処理時間:'.($now - $st). '秒<br>\n');
 
     return view('twitter.index')->
           with(['getTweets' => $tweets , 'inputURL' => $request->searchURL ,
